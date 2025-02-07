d4f1bd034b5f8e29214d56b7ab9ab505afe2f023
schmidsebas <sebastian.schmid@baywa.de>
2025-02-07 14:26:08 +0100

    first init

---
+++ makeexe.ps1
@@ -0,0 +1,2 @@
+﻿#Install-Module -Name ps2exe 
+Invoke-PS2EXE -InputFile D:\users\schmidsebas\GitHub\KeepTeamsGreen\makeexe.ps1  -OutputFile D:\users\schmidsebas\GitHub\KeepTeamsGreen\keepmoving.ps1
