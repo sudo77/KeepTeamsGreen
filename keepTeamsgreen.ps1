c567024404909a6f6dba838ccabac108b435b247
sudo <sudo77@me.com>
2025-02-12 15:26:49 +0100

    Bewege die Maus alle 120 Sekunden um 1 Pixel hin und her

--- keepTeamsgreen.ps1
+++ keepTeamsgreen.ps1
@@ -9,7 +9,6 @@ $frames = @(
 )
 
 # Display "keep moving..." text
-Clear-Host
 Write-Host "keep moving..." -ForegroundColor green
 
 # Initialize the counter for the number of hashes
@@ -21,6 +20,7 @@ for ($i = 0; $i -lt 12; $i++) {
     while ((Get-Date) - $startTime -lt [TimeSpan]::FromSeconds(10)) {
         foreach ($frame in $frames) {
             Clear-Host
+            Write-Host  "Bewege die Maus alle 120 Sekunden um 1 Pixel hin und her" -ForegroundColor green
             Write-Host "keep moving..." -ForegroundColor green
             Write-Host $hashes -ForegroundColor Green
             Write-Host (" " * $i + $frame) -NoNewline -ForegroundColor Green
@@ -55,6 +55,7 @@ while ($true) {
         while ((Get-Date) - $innerStartTime -lt [TimeSpan]::FromSeconds(10)) {
             foreach ($frame in $frames) {
                 Clear-Host
+                Write-Host  "Bewege die Maus alle 120 Sekunden um 1 Pixel hin und her" -ForegroundColor green
                 Write-Host "keep moving..." -ForegroundColor green
                 Write-Host $hashes -ForegroundColor Green
                 Write-Host $frame -NoNewline -ForegroundColor Green
